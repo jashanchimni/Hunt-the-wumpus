a05915730d2f19ada07d3978a962ac1160d9ede8
jashanchimni <1083958@apps.nsd.org>
2024-02-12 14:18:46 -0800

    First commit

--- JashanChimni.java
+++ JashanChimni.java
@@ -2,5 +2,6 @@ public class JashanChimni {
 
     public static void JashanChimni(String[] args){
         System.out.println("This is my file!");
+        System.out.println("Hello, I have passed the step, configuration is complete");
     }
 }
