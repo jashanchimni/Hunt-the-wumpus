005f531213432fada00d77cf04f6ba70eacbfd8f
CameronBerry1 <1039194@apps.nsd.org>
2024-03-22 14:26:44 -0700

    cave changes

---
+++ Cave/Cave.java
@@ -0,0 +1,33 @@
+package Cave;
+// Cameron Berry
+public class Cave{
+
+  // PROPERTIES //
+
+  // CONSTRUCTOR //
+  public Cave(){
+    // empty
+  }
+  // METHODS // 
+  public void enterCave(){
+    // empty
+  }
+
+  public void exitCave(){
+    // empty
+  }
+
+  public void getHazard(){
+    // empty
+  }
+
+  public void getWalls(){
+    // empty
+  }
+
+  public void getLocation(){
+    // empty
+  }
+
+
+}
---
+++ Cave/CaveExam.java
@@ -0,0 +1,5 @@
+package Cave;
+
+public class CaveExam {
+    
+}
