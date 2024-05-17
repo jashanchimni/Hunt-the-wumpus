f8f730519ba27211086fc049c7e9389d493626e9
199NO <Nobear08@outlook.com>
2024-05-17 13:58:05 -0700

    Sounds

--- Main.java
+++ Main.java
@@ -53,7 +53,8 @@ class Main{
   public static void main(String[] args) {
 
     //WHEN EVER YOU WANT TO PLAY A SOUND USE THIS FORMAT
-    SoundTesting.playSound("gameboy-advance-startup-sound-made-with-Voicemod.wav");
+    
+    SoundTesting.playSound("!GameboySound.wav");
     // ---------------------------------------------------------------------------\\
     
      Scanner user = new Scanner(System.in);            // Creating A Scanner 
