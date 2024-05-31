eb1437b704b4c44e372be9a05f21dc5d1ff701ba
jashanchimni <1083958@apps.nsd.org>
2024-05-31 13:56:30 -0700

    made trivia

--- Trivia.java
+++ Trivia.java
@@ -1,11 +1,58 @@
-import java.util.ArrayList;
+/* 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Random;
-import java.io.File;
-import java.io.FileNotFoundException;
 
 public class Trivia{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the TriviaMaster!");
 
-    File triviaFile;
-    File hintFile;
+        System.out.println("Choose a rank of questions to answer(1-5): ");
+        int selectedRank = scanner.nextInt();
+
+        JSONObject triviaData = readJSONFile("trivia.json");
+        JSONArray triviaArray = (JSONArray) triviaData.get("trivia");
+        JSONArray selectedQuestions = new JSONArray();
+
+        for (Object obj : triviaArray) {
+            JSONObject question = (JSONObject) obj;
+            long rank = (long) question.get("Rank");
+            if(rank == selectedRank){
+                selectedQuestions.add(question);
+            }
+        }
+
+        if (selectedQuestions.size() > 0) {
+            JSONObject randomQuestion = (JSONObject) selectedQuestions.get(new Random().nextInt(selectedQuestions.size()));
+            String questionText = (String) randomQuestion.get("question");
+            JSONArray choices = (JSONArray) randomQuestion.get("choices");
+            int correctAnswerIndex = ((Long) randomQuestion.get("Key")).intValue();
+            System.out.println("Question: " + questionText);
+            for(int i = 0; i < choices.size(); i++){
+                System.out.println(i + "i " + choices.get(i));
+            }
+
+            System.out.println("Enter the number of your answer(Starts at 0): ");
+            int userAnswer = scanner.nextInt();
+
+            if(userAnswer == correctAnswerIndex){
+                int coinsEarned = selectedRank * 10;
+                System.out.println("Congratulations, you answered it correctly and recieved " + coinsEarned + " coins! ");
+            } else {
+                System.out.println("Sorry, you got it wrong. Better luck next time!");
+            }
+
+        } else {
+            System.out.println("That is an invalid rank, please select a valid rank");
+
+        }
+
+        
+    }
 }
+
+*/
