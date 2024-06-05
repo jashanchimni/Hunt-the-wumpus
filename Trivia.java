438833882c9f0cff7a520bbac79aec55eb162bdb
jashanchimni <1083958@apps.nsd.org>
2024-06-05 13:11:12 -0700

    comments

--- Trivia.java
+++ Trivia.java
@@ -1,4 +1,4 @@
- 
+ //libraries
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,16 +8,21 @@ import java.util.Random;
 
 public class Trivia{
     public static void main() {
+
+        //initializes scanner and prints intro message
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the TriviaMaster!");
 
+        //user selects rank based on difficulty of question
         System.out.println("Choose a rank of questions to answer(1-5): ");
         int selectedRank = scanner.nextInt();
 
+        //variables are set for the trivia that is being taken from the JSON File
         JSONObject triviaData = readJSONFile("trivia.json");
         JSONArray triviaArray = (JSONArray) triviaData.get("trivia");
         JSONArray selectedQuestions = new JSONArray();
 
+        //Makes a new array out of the questions from the selected rank 
         for (Object obj : triviaArray) {
             JSONObject question = (JSONObject) obj;
             long rank = (long) question.get("Rank");
@@ -26,6 +31,7 @@ public class Trivia{
             }
         }
 
+        //Takes random question and stores the options, key, etc 
         if (selectedQuestions.size() > 0) {
             JSONObject randomQuestion = (JSONObject) selectedQuestions.get(new Random().nextInt(selectedQuestions.size()));
             String questionText = (String) randomQuestion.get("question");
@@ -36,6 +42,7 @@ public class Trivia{
                 System.out.println(i + ": " + choices.get(i));
             }
 
+            //user enters information and feedback is given based on response
             System.out.println("Enter the number of your answer(Starts at 0): ");
             int userAnswer = scanner.nextInt();
 
